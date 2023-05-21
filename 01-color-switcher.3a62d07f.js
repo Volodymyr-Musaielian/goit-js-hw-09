9496907f3cfe472367342cc5c2db0fd937790b8b
Volodymyr-Musaielian <121056577+Volodymyr-Musaielian@users.noreply.github.com>
2023-05-21 13:56:01 +0000

    Deploying to gh-pages from @ Volodymyr-Musaielian/goit-js-hw-09@6921560f849ef6164484e48c8d2fff2ecd653f7f 🚀

---
+++ 01-color-switcher.3a62d07f.js
@@ -0,0 +1,2 @@
+const t={startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]"),body:document.querySelector("body")};t.startBtn.addEventListener("click",(function(){e=setInterval((()=>{t.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),e&&(t.startBtn.disabled=!0);t.stopBtn.disabled=!1})),t.stopBtn.addEventListener("click",(function(){clearInterval(e),e&&(t.startBtn.disabled=!1);t.stopBtn.disabled=!0}));let e=null;
+//# sourceMappingURL=01-color-switcher.3a62d07f.js.map
